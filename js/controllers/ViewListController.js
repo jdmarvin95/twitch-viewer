6b77900b37d3379e6e633636dab0bf05ae8830c8
ASBOWalrus <joemarvin95@gmail.com>
2017-10-15 11:16:24 +0100

    Added remove button tooltip, ability to press enter in add box to add streamer

--- js/controllers/ViewListController.js
+++ js/controllers/ViewListController.js
@@ -1,8 +1,14 @@
 app.controller('ViewListController', ['$scope', 'GetTwitchInfo', function($scope,  GetTwitchInfo) {
 	$scope.streamers = GetTwitchInfo.streamList
 
+	document.getElementById('add-name-box').onkeypress = function(e) {
+		if(e.keyCode == 13) {
+			document.getElementById("add-name-btn").click()
+		}
+	}
+
 	$scope.addStreamer = function() {
-		var val = document.getElementById('add-name').value
+		var val = document.getElementById('add-name-box').value
 		GetTwitchInfo.pushToStreamerArr(val)
 	}
 
